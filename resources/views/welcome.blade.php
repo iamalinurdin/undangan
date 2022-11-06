1a60fe0cd08b6c023be46aca1d0252ffac4b8895
Ali Nurdin <alinurdin159@gmail.com>
2022-11-06 13:26:28 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -136,6 +136,12 @@
                 </div>
             </div>
         </div>
+
+        <footer class="main-color text-center">
+            <span class="fw-bold">Powered by</span>
+            <h5 class="fw-bold" style="color: #cc2663;">PT. TOBIAS DIGITAL INDONESIA</h5>
+            <img src="{{ asset('logo.png') }}" alt="">
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
